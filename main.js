ef0d603a28a958444b6c0528e800ef89378a3f1e
jrejaud <jrejaud@gmail.com>
2015-09-07 17:17:12 -0700

    added scheduler

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var parser = require('cron-parser');
 var childProcess = require('child_process');
 // var emailer = require(__dirname+"/emailer.js")
 
-var FOUR_AM_CHRON_JOB = "0 5 * * *";
+// var FOUR_AM_CHRON_JOB = "0 5 * * *";
 
 function calendarUpdate() {
   var process = childProcess.fork(__dirname+"/update_google_calendar.js");
@@ -19,21 +19,21 @@ function calendarUpdate() {
   });
 }
 
-function showNextCronJob() {
-
-  var interval = parser.parseExpression(FOUR_AM_CHRON_JOB);
-
-  console.log("Setting up chron job, next calendar update: "+interval.next());
-
-}
+// function showNextCronJob() {
+//
+//   var interval = parser.parseExpression(FOUR_AM_CHRON_JOB);
+//
+//   console.log("Setting up chron job, next calendar update: "+interval.next());
+//
+// }
 
 console.log("San Francisco City Guides Calendar Main app");
 console.log("Running update once");
 
 calendarUpdate();
-showNextCronJob();
-
-var j = schedule.scheduleJob(FOUR_AM_CHRON_JOB, function(){
-    console.log('It is 1am PST (4am EST since that is where the server runs), it\'s time to update the google calendar!');
-    calendarUpdate();
-});
+// showNextCronJob();
+//
+// var j = schedule.scheduleJob(FOUR_AM_CHRON_JOB, function(){
+//     console.log('It is 1am PST (4am EST since that is where the server runs), it\'s time to update the google calendar!');
+//     calendarUpdate();
+// });
